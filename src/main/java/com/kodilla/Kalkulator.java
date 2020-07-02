f82461614f009d78612e8e333b69cbaf59d2d2d6
PaulinaSlowik <slowik.paulina92@gmail.com>
2020-07-02 14:12:07 +0200

    First Commit

--- src/main/java/com/kodilla/Kalkulator.java
+++ src/main/java/com/kodilla/Kalkulator.java
@@ -1,4 +1,19 @@
 package com.kodilla;
 
 public class Kalkulator {
+    int a;
+    int b;
+
+    public int additionMethod(int a, int b) {
+        return a+b;
+    }
+    public int subtractionMethod(int a, int b) {
+        return a-b;
+    }
+
+    public static void main(String args []) {
+        Kalkulator calculator = new Kalkulator();
+        System.out.println(calculator.additionMethod(5,6));
+        System.out.println(calculator.subtractionMethod(10,2));
+    }
 }
